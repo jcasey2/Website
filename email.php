c2037bb119cd028c9298f16dff9b6e60cd881099
John <jcasey2@mail.usf.edu>
2016-06-02 12:16:51 -0400

    Beefed up email.php, needs eror checking and testing

--- email.php
+++ email.php
@@ -1,30 +1,50 @@
 <?php
 
-//set up email
-$to = 'hello@johnrcasey.net';
-$subject = $_POST['subject'];
-
-// Remove all illegal characters from email
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
-$message = 'Name: ' . $_POST['first_name'] . ' ' . $_POST['last_name'] . "\r\n\r\n";
-$message .= 'Email: ' . $email . "\r\n\r\n";
-$message .= 'Message: ' . $_POST['message'];
-$headers = 'From: ' . $_POST['email'] . '\r\n';
-$headers .= 'Content-Type: text/plain; charset=utf-8';
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-
-$success = mail($to, $subject, $message, $headers);
-
-if($success)
-{
-	//header("Location: http://johnrcasey.net/contact.html");
-	header("Location: localhost/contact.html");
+function test_input($data){
+	$data = trim($data);
+	$data = stripcslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 }
-else
-{
-	echo "Email was not sent successfully";
-	echo "<a href='/contact.html'>Back</a>";
+
+//Make sure the requst is actually a POST
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	//Scrub for malicious attacks
+	$fname = test_input($_POST['first_name']);
+	$lname = test_input($_POST['last_name']);
+	$email = test_input($_POST['email']);
+	//$email = 
+	$subject = test_input($_POST['subject']);
+	$message = test_input($_POST['message']);
+
+	//set up email
+	$to = 'hello@johnrcasey.net';
+
+	$message = 'Name: ' . $fname . ' ' . $lname . "\r\n\r\n";
+	$message .= 'Email: ' . $email . "\r\n\r\n";
+	$message .= 'Message: ' . $message;
+
+	//wordwrap used to keep lnes to 80 charactors
+	$message = wordwrap($message, 80, "\r\n")
+
+	$headers = 'From: ' . $email . '\r\n';
+	$headers .= 'Content-Type: text/plain; charset=utf-8';
+
+	$success = mail($to, $subject, $message, $headers);
+
+	if($success)
+	{
+		header("Location: /contact.html");
+	}
+	else
+	{
+		echo "Email was not sent successfully\r\n";
+		echo "<a href='/contact.html'>Back</a>";
+	}
+}
+else{
+	echo "This was not a POST message!"
 }
 
 ?>
